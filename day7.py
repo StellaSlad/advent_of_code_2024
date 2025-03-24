351a7e7a100a7f05393c7d40c559a96f5ee658ac
Stella Slad <127109492+StellaSlad@users.noreply.github.com>
2025-03-24 11:14:42 +1100

    Added problem description

--- day7.py
+++ day7.py
@@ -1,3 +1,21 @@
+""" Problem description:
+
+Input example:
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+
+Each line in the input represents a single equation. The test value appears before the colon on each line; 
+it is your job to determine whether the remaining numbers can be combined with mathematical operators ("+" or "*") to produce the test value.
+Operators are always evaluated left-to-right, not according to precedence rules; numbers in the equations cannot be rearranged. 
+The puzzle answer is the sum of the values of all the equations that could possibly be true."""
+
 import sys
 import numpy as np
 from itertools import product
